54cf52184c277f8489f6759488c5a59e62ce7f7f
DigitalG <Thesever99@gmail.com>
2019-11-13 11:52:15 +0200

    'developmenbt'

--- cell.js
+++ cell.js
@@ -1,54 +1,66 @@
-get_color_string = (color) => {
-	return `rgb(${color[0]}, ${color[1]}, ${color[2]})`
-}
-
 //Color modes:
-//0 - this.color
-//1 - this.is_filled
+//0 - this.color Show current color
+//1 - this.is_filled Show answers
 
 function Cell(x, y, size,
-					color=[255, 255, 255],
-					is_filled=0, 
-					fill_color=[0,0,0], 
-					rigth_color=[128, 128, 128], 
-					empty_color=[255,255,255]){
-	this.x = x
-	this.y = y
-	this.size = size
-	this.color = get_color_string(color)
-	this.empty_color = get_color_string(empty_color)
-	this.fill_color = get_color_string(fill_color)
-	this.rigth_color = get_color_string(rigth_color)
-	this.is_filled = is_filled
-	this.isClicked = 0
+              is_filled = 0,
+              empty_color = color(255, 255, 255),
+              fill_color = color(0, 0, 0),
+              right_color = color(128, 128, 128)) {
 
-	this.on_draw = (color_mode) => {
-		stroke(10)
-		if(color_mode === 0)
-			fill(this.color)
-		else if(color_mode === 1){
-			fill(this.is_filled===0?this.empty_color:this.fill_color)
-		}
-		square(this.x, this.y, this.size)
-		fill(color_mode===0?0:(this.is_filled===1?this.empty_color:this.fill_color))
-		//text(this.is_filled,this.x + this.size/2, this.y + this.size/2)
-	}
+    this.x = x //X grid position
+    this.y = y //Y grid position
+    this.size = size //cell size in pixels
+    this.current_color = empty_color //Current cell color
+    this.hasX = false //IF X on cell is needed
+    this.empty_color = empty_color //Color to show when cell is empty
+    this.fill_color = fill_color //Color to show when cell is checked
 
-	this.on_click = click_mode => {
-		console.log(click_mode)
-		if(this.isClicked === 0){
-			this.isClicked = 1
-			if(this.is_filled === click_mode){
-				
-			} else {
-				document.getElementById('mistakes').value++
-			}
-			this.color = this.is_filled===0?this.fill_color:this.rigth_color
-		}
-	}
+    this.color_fade_to = color(0,0,0)
+    this.color_fade_delta = color(0,0,0)
+    this.fade_ticks = 0
 
-	this.set_color = color =>{
-		this.color_delta = [(this.color[0]-color[0])/60, (this.color[1]-color[1])/60, (this.color[2]-color[2])/60]
-		this.next_color = color
-	}
+    this.is_filled = is_filled //IF this cell needs to be filled
+    this.isClicked = 0
+
+
+    this.on_draw = (color_mode) => {
+        if(this.fade_ticks > 1){
+            this.current_color.setRed(red(this.current_color) + red(this.color_fade_delta))
+            this.current_color.setGreen(green(this.current_color) + green(this.color_fade_delta))
+            this.current_color.setBlue(blue(this.current_color) + blue(this.color_fade_delta))
+            this.fade_ticks--
+            console.log('tick')
+        }
+
+        stroke(10)
+        fill(this.current_color)
+        square(this.x, this.y, this.size)
+        if(this.hasX) {
+            line(this.x, this.y, this.x + this.size, this.y + this.size)
+            line(this.x + this.size, this.y, this.x, this.y + this.size)
+        }
+    }
+
+    this.on_click = click_mode => {
+        this.fade_to_color(color('green'), 1)
+        if (this.isClicked === 0) {
+            this.isClicked = 1
+            if (this.is_filled === click_mode) {
+
+            } else {
+                document.getElementById('mistakes').value++
+            }
+            this.color = this.is_filled === 0 ? this.fill_color : this.rigth_color
+        }
+    }
+
+    this.fade_to_color = (color, fade_time) => {
+        this.fade_ticks = fade_time*60
+        this.color_fade_to = color
+        this.color_fade_delta.setRed((red(this.current_color) - red(color)))
+        this.color_fade_delta.setGreen((green(this.current_color) - green(color)))
+        this.color_fade_delta.setBlue((blue(this.current_color) - blue(color)))
+    }
+
 }
--- sketch.js
+++ sketch.js
@@ -20,87 +20,100 @@ let y_numbers = []
 //1 - X-mode
 let click_mode = 0
 
-const changeColorMode = () =>{
-	color_mode = color_mode===1?0:1
-	console.log(color_mode)
+const changeColorMode = () => {
+    color_mode = color_mode === 0 ? 1 : 0
+    console.log(color_mode)
+}
+
+const changeClickMode = (isFill) => {
+    click_mode = isFill?0:1
 }
 
 const getRowValue = (row) => {
-	result = []
-	temp = 0
-	row.forEach(num => {
-		if(num === 0)
-		{
-			if(temp !== 0){
-				result.push(temp)
-				temp = 0
-			}
-		}else{
-			temp++
-		} 
-	})
-	if(temp!==0){
-		result.push(temp)
-	}
-	return result
+    result = []
+    temp = 0
+    row.forEach(num => {
+        if (num === 0) {
+            if (temp !== 0) {
+                result.push(temp)
+                temp = 0
+            }
+        } else {
+            temp++
+        }
+    })
+    if (temp !== 0) {
+        result.push(temp)
+    }
+    return result
 }
 
-const checkGrid = () =>{
-	
+const checkGrid = () => {
+
 }
 
 
-function setup(){
-	createCanvas(canvas_size+number_padding+margin*2, canvas_size+number_padding+margin*2)
+function setup() {
+    //Color settings
+    let color_settings = {
+        'default_color': color(255, 255, 255),
+        'fill_color': color(0, 0, 0),
+        'check_color': color(233, 0, 255)
+    }
 
-	cell_size = canvas_size/grid_size
+    //Canvas setup
+    createCanvas(canvas_size + number_padding + margin * 2, canvas_size + number_padding + margin * 2)
 
-	for(let x = 0; x<grid_size; x++){
-		for(let y = 0; y<grid_size; y++){
-			grid_cells.push(new Cell(x*cell_size+margin+number_padding,
-									 y*cell_size+margin+number_padding,
-									 cell_size,
-									 color=[255,255,255],
-									 is_filled=random()>0.5?1:0))
-		}
-	}
+    cell_size = canvas_size / grid_size
 
-	temp_num = []
-	for(let x = 0; x<grid_size; x++){
-		for(let y = 0; y<grid_size; y++){
-			temp_num.push(grid_cells[x*grid_size + y].is_filled)
-		}
-		x_numbers.push(getRowValue(temp_num).reverse())
-		temp_num = []
-	}
+    for (let x = 0; x < grid_size; x++) {
+        for (let y = 0; y < grid_size; y++) {
+            grid_cells.push(new Cell(x * cell_size + margin + number_padding,
+                y * cell_size + margin + number_padding,
+                cell_size,
+                random() > 0.5 ? 1 : 0,
+                color_settings.default_color))
+        }
+    }
 
-	for(let y = 0; y<grid_size; y++){
-		for(let x = 0; x<grid_size; x++){
-			temp_num.push(grid_cells[x*grid_size + y].is_filled)
-		}
-		y_numbers.push(getRowValue(temp_num))
-		temp_num = []
-	}
+    temp_num = []
+    for (let x = 0; x < grid_size; x++) {
+        for (let y = 0; y < grid_size; y++) {
+            temp_num.push(grid_cells[x * grid_size + y].is_filled)
+        }
+        x_numbers.push(getRowValue(temp_num).reverse())
+        temp_num = []
+    }
+
+    for (let y = 0; y < grid_size; y++) {
+        for (let x = 0; x < grid_size; x++) {
+            temp_num.push(grid_cells[x * grid_size + y].is_filled)
+        }
+        y_numbers.push(getRowValue(temp_num))
+        temp_num = []
+    }
 }
 
-function draw(){
-	background(0)
-	grid_cells.forEach(cell => {cell.on_draw(color_mode)})
-	let x_delta = 0
-	for(let i =0; i<grid_size;i++){
-		fill('white')
-		text(y_numbers[i], margin ,number_padding+margin+cell_size/2+cell_size*i)
-		x_numbers[i].forEach(num =>{
- 			text(num, number_padding+margin+cell_size/2+cell_size*i, number_padding-x_delta)
-			x_delta+=15
-		})
-		x_delta = 0
-	}
+function draw() {
+    background(0)
+    grid_cells.forEach(cell => {
+        cell.on_draw(color_mode)
+    })
+    let x_delta = 0
+    for (let i = 0; i < grid_size; i++) {
+        fill('white')
+        text(y_numbers[i], margin, number_padding + margin + cell_size / 2 + cell_size * i)
+        x_numbers[i].forEach(num => {
+            text(num, number_padding + margin + cell_size / 2 + cell_size * i, number_padding - x_delta)
+            x_delta += 15
+        })
+        x_delta = 0
+    }
 }
 
-function mouseClicked(){
-	let x = Math.trunc((mouseX - number_padding - margin)/(canvas_size/grid_size));
-	let y = Math.trunc((mouseY - number_padding - margin)/(canvas_size/grid_size));
-	grid_cells[grid_size*x + y].on_click(click_mode)
-	checkGrid()
+function mouseClicked() {
+    let x = Math.trunc((mouseX - number_padding - margin) / (canvas_size / grid_size));
+    let y = Math.trunc((mouseY - number_padding - margin) / (canvas_size / grid_size));
+    grid_cells[grid_size * x + y].on_click(click_mode)
+    checkGrid()
 }
